77c5a567d8ebb62d2d024184a33cfb6a39ae4815
Promi Jarin Tasnim <orick@Ks-MacBook-Pro.local>
2021-09-02 07:16:49 +0800

    spinner

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
+// Load data on click
 const loadBooks = () => {
 	const searchField = document.getElementById('search-input');
 	const searchText = searchField.value;
-	// searchField.value = '';
-
-	// fetch('http://openlibrary.org/search.json?q=javascript')
-	fetch(`http://openlibrary.org/search.json?q=${searchText}`)
-		.then((response) => response.json())
-		.then((data) => displayAll(data.docs));
-	// .then((data) => console.log(data.docs[0]));
+	searchField.value = '';
+	if (searchText === '') {
+		alert('Please type the book name you are looking for');
+		displaySpinner('none');
+	} else {
+		fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+			.then((response) => response.json())
+			.then((data) => displayAll(data.docs));
+		displaySpinner('block');
+	}
 };
+
+// display all information in a card
 const displayAll = (bookDetails) => {
-	// console.log(books);
 	const displayResult = document.getElementById('display-result');
+	let count = document.getElementById('count');
+	count.innerHTML = `Total books found ${bookDetails.length}`;
+
+	displayResult.textContent = '';
 	bookDetails.forEach((book) => {
 		const bookDetailDiv = document.createElement('div');
 		bookDetailDiv.classList.add('col');
 		bookDetailDiv.innerHTML = `
-		     <div class="card h-100">
-		<img class="card-img-top img-fluid"
+		     <div class="card h-100 ">
+		<img class="card-img-top img-fluid h-50 w-75 mx-auto my-2 shadow-lg"
         src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="">
 		            <div class="card-body">
 		                <h3 class="card-title">${book.title}</h3>
@@ -33,15 +42,24 @@ const displayAll = (bookDetails) => {
 												} 
                                                 </p>
                         <p><b>Publisher Name: </b>
-                        ${book.publisher ? book.publisher : 'Unavailable'}</p>
+                        ${
+													book.publisher ? book.publisher : 'No information'
+												}</p>
                         <p><b>Related Tags:</b> <br>
                         ${
 													book.subject
 														? book.subject.slice(1, 5)
-														: 'Noting Related'
+														: 'Nothing is related'
 												}</p>
 		            </div>
 		    `;
 		displayResult.appendChild(bookDetailDiv);
 	});
+	displaySpinner('none');
+};
+
+// spinner function
+const displaySpinner = (viewOnPage) => {
+	const spinner = document.getElementById('spinner');
+	spinner.style.display = viewOnPage;
 };
